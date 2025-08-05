99d36b6fa57299381d3b0a97189a7bb21d3a33e8
Kailash Rawat <kailash2005rawat@gmail.com>
2025-08-05 18:05:15 +0530

    update

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -3,7 +3,7 @@ const app = require("./src/app")
 const connect = require("./src/db/db")
 const PORT = process.env.PORT || 3000
 
-app.listen(3000,()=>{
-    console.log("..SERVER RUNNING AT PORT NUMBER :",3000)
+app.listen(PORT,()=>{
+    console.log("..SERVER RUNNING AT PORT NUMBER :",PORT)
     connect()
 })
